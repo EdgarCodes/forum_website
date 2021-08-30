e4883d70c6f8aa6617514f9483a4cb08babc10c0
Edgar Gonzalez <Edgar.gonzalez801@topper.wku.edu>
2021-08-29 19:05:10 -0500

    Added mariadb support and added env variables

--- forum_website/forum_website/settings.py
+++ forum_website/forum_website/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5c0q_smd5e!ga$5lx9+m^4-%@jdyb7i*-h(l%5cdm6rnt)%^zu'
+SECRET_KEY = config("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'forum_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'forum_site',
+        'USER': 'e0g05db',
+        'PASSWORD': config("MYSQL_PASS"),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
