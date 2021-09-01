2df1908d8d8981d12367f8e2daba2f4e9476fd9e
Edgar Gonzalez <Edgar.gonzalez801@topper.wku.edu>
2021-08-31 19:06:16 -0500

    fixed page's does not exist issue

--- forum_website/forum_website/urls.py
+++ forum_website/forum_website/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from pages.views import home_view,about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
